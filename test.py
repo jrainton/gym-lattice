3d55b486b6d7ff875998227f2db82273fdc23eaf
Jay Rainton <jayrainton@gmail.com>
2021-10-22 13:15:06 -0700

    updating test with DQN

--- test.py
+++ test.py
@@ -2,19 +2,42 @@ from gym_lattice.envs import Lattice2DEnv
 from gym import spaces
 import numpy as np
 
+from stable_baselines3 import DQN
+from stable_baselines3.common.evaluation import evaluate_policy
+
+
 np.random.seed(42)
 
 seq = 'HHPHH' # Our input sequence
 action_space = spaces.Discrete(4) # Choose among [0, 1, 2 ,3]
 env = Lattice2DEnv(seq)
 
-for i_episodes in range(5):
-    env.reset()
-    while not env.done:
-        # Random agent samples from action space
-        action = action_space.sample()
-        obs, reward, done, info = env.step(action)
-        env.render()
-        if done:
-            print("Episode finished! Reward: {} | Collisions: {} | Actions: {}".format(reward, info['collisions'], info['actions']))
-            break
+# Instantiate the agent
+model = DQN('MlpPolicy', env, verbose=1)
+# Train the agent
+model.learn(total_timesteps=int(2e5))
+# Save the agent
+model.save("dqn_lattice")
+del model  # delete trained model to demonstrate loading
+
+# Load the trained agent
+# NOTE: if you have loading issue, you can pass `print_system_info=True`
+# to compare the system on which the model was trained vs the current one
+# model = DQN.load("dqn_lunar", env=env, print_system_info=True)
+model = DQN.load("dqn_lattice", env=env)
+
+# Evaluate the agent
+# NOTE: If you use wrappers with your environment that modify rewards,
+#       this will be reflected here. To evaluate with original rewards,
+#       wrap environment in a "Monitor" wrapper before other wrappers.
+mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
+
+# Enjoy trained agent
+obs = env.reset()
+while not env.done():
+    action, _states = model.predict(obs, deterministic=True)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+    if dones:
+        print("Episode finished! Reward: {} | Collisions: {} | Actions: {}".format(reward, info['collisions'], info['actions']))
+        break

05a8a5c4768ff91a58a9ca3e1b9bb8035690ec42
Jay Rainton <jayrainton@gmail.com>
2021-10-22 13:38:27 -0700

    fixing

--- test.py
+++ test.py
@@ -33,8 +33,8 @@ model = DQN.load("dqn_lattice", env=env)
 mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 
 # Enjoy trained agent
-env.reset()
-while not env.done():
+obs = env.reset()
+while not env.done:
     action, _states = model.predict(obs, deterministic=True)
     obs, rewards, dones, info = env.step(action)
     env.render()

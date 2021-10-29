2e6b13eebab7d93b1187986f63660af16d238652
Jay Rainton <jayrainton@gmail.com>
2021-10-29 16:46:53 -0700

    immediate reward:

--- gym_lattice/envs/lattice2d_env.py
+++ gym_lattice/envs/lattice2d_env.py
@@ -382,7 +382,11 @@ class Lattice2DEnv(gym.Env):
 
         print(self.previous_state)
         print(self.state)
-        state_reward = self._compute_free_energy(self.state) if self.done else 0
+
+        previous_gibbs = self._compute_free_energy(self.previous_state)
+        current_gibbs = self._compute_free_energy(self.state)
+        state_reward = current_gibbs - previous_gibbs 
+        #state_reward = self._compute_free_energy(self.state) if self.done else 0
         collision_penalty = self.collision_penalty if collision else 0
         actual_trap_penalty = -floor(len(self.seq) * self.trap_penalty) if is_trapped else 0
 
